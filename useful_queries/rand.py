61cf984c979e3dbce946c6df4b32328206670c2d
ksnikiforov <kirnik77@gmail.com>
2020-06-13 01:37:44 +0300

    clean up

--- useful_queries/rand.py
+++ useful_queries/rand.py
@@ -1,10 +1,12 @@
 import random
-genre = ['Drama', 'Comedy', 'Adventure', 'Horror', 'School', 'University', 'Fantasy', 'Sci-Fi',
-        'Science', 'Fighting', 'Romance', 'Animals']
+genre = ['Drama', 'Comedy', 'Adventure', 'Horror', 'School', 
+         'University', 'Fantasy', 'Sci-Fi', 'Science', 
+         'Fighting', 'Romance', 'Animals']
 
 def gen():
-    for i in range(1, 401):
-        print('insert into genre(genre, comic_id) values (\'{0}\', {1});'.format(genre[random.randint(0,10)], i))
+    for i in range(200):
+        print("insert into genre(genre, comic_id) values ('{0}', {1}) on conflict "
+              "(genre, comic_id) DO NOTHING;".format(random.choice(genre), random.randint(1, 400)))
 
             
 gen()
